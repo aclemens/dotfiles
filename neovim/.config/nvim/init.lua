a8a13cbbeee149786816deec20b0c3413fd9c5f9
Andi Clemens <andi.clemens@gmail.com>
2024-12-06 15:03:22 +0100

    add base plugins to neovim (gruvbox, nvim-tree, lualine)

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,18 +1,4 @@
-vim.cmd [[colorscheme vim]]
-vim.opt.termguicolors = false
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-vim.opt.expandtab = true
-vim.opt.laststatus = 1
-vim.opt.autoread = true
-vim.opt.autowrite = true
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+require("core/keymaps")
+require("core/plugins")
+require("core/plugin_config")
 
---
+++ neovim/.config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,15 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.laststatus = 2
+vim.opt.autoread = true
+vim.opt.autowrite = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
