4cee4337b7f225b7343b6ddd1dea5c04b46b3137
Andi Clemens <andi.clemens@gmail.com>
2024-12-06 12:28:14 +0100

    initial commit of neovim config

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,19 @@
+vim.cmd [[colorscheme vim]]
+vim.opt.termguicolors = false
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.laststatus = 1
+vim.opt.autoread = true
+vim.opt.autowrite = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
