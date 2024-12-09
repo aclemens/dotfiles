2b6976627b0f3ac47aa1b9f4e249f58bff386ccb
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 14:27:26 +0100

    split keymaps and options

---
+++ neovim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,14 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.laststatus = 2
+vim.opt.autoread = true
+vim.opt.autowrite = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
