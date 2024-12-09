474cf0c1056f7d2d5c85ec2fecf01886a36c3c72
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 19:02:08 +0100

    add description to keymaps

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,18 +1,18 @@
 local keymap = vim.keymap -- for conciseness
 
 -- reset search highlight
-keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = "remove search highlight" })
 
 -- split windows
-keymap.set('n', '<leader>sv', '<C-w>v') --split vertically
-keymap.set('n', '<leader>sh', '<C-w>s') -- split horizontally
-keymap.set('n', '<leader>se', '<C-w>=') -- make split equal
-keymap.set('n', '<leader>sx', '<cmd>close<CR>') -- close current
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = "Split window vertically" })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = "Split window horizontally" })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = "Make split equal" })
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = "close the current window" })
 
 -- tab windows
-keymap.set('n', '<leader>to', '<cmd>tabnew<CR>') -- open tab
-keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>') -- close tab
-keymap.set('n', '<leader>tp', '<cmd>tabp<CR>') -- previous tab
-keymap.set('n', '<leader>tn', '<cmd>tabn<CR>') -- next tab
-keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>') -- open current buffer in new tab
+keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = "open new tab" })
+keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = "close tab" })
+keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = "previous tab" })
+keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = "next tab" })
+keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = "open current buffer in new tab" })
 
--- neovim/.config/nvim/lua/plugins/lazygit.lua
+++ neovim/.config/nvim/lua/plugins/lazygit.lua
@@ -2,6 +2,6 @@ return {
   "kdheepak/lazygit.nvim",
 
   config = function()
-    vim.keymap.set('n', '<leader><leader>', ':LazyGit<CR>')
+    vim.keymap.set('n', '<leader><leader>', ':LazyGit<CR>', { desc = "Open LazyGit" })
   end
 }
