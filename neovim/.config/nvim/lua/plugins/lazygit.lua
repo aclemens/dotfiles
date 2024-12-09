33ca29abe2e4b27b31f7b94cf48785566f9bc18c
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 21:53:11 +0100

    add optional dependencies for lazygit plugin

--- neovim/.config/nvim/lua/plugins/lazygit.lua
+++ neovim/.config/nvim/lua/plugins/lazygit.lua
@@ -1,5 +1,8 @@
 return {
   "kdheepak/lazygit.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
 
   config = function()
     vim.keymap.set('n', '<leader><leader>', ':LazyGit<CR>', { desc = "Open LazyGit" })
