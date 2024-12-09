4300531f2ab5688d52ef7b72c7b2c0ddcde0b663
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 18:21:22 +0100

    change lazygit keymap to 2x<leader>

--- neovim/.config/nvim/lua/plugins/lazygit.lua
+++ neovim/.config/nvim/lua/plugins/lazygit.lua
@@ -1,6 +1,6 @@
 return {
   "kdheepak/lazygit.nvim",
   config = function()
-    vim.keymap.set('n', '<leader>l', ':LazyGit<CR>')
+    vim.keymap.set('n', '<leader><leader>', ':LazyGit<CR>')
   end
 }
