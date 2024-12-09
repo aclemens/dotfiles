25d8857e36e6c5bbc928da01dd142e20377ae572
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 14:32:32 +0100

    cleanup options file

--- neovim/.config/nvim/lua/core/options.lua
+++ neovim/.config/nvim/lua/core/options.lua
@@ -1,7 +1,8 @@
+vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
-vim.opt.expandtab = true
+
 vim.opt.laststatus = 2
 vim.opt.autoread = true
 vim.opt.autowrite = true
