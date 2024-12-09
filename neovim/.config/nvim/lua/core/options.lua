55d2f55de962fcbc3c3aaba31f64aaeca091baa7
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 14:37:27 +0100

    add search settings

--- neovim/.config/nvim/lua/core/options.lua
+++ neovim/.config/nvim/lua/core/options.lua
@@ -10,6 +10,9 @@ vim.opt.autowrite = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
