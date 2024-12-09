a53cc7753674ac8966d210e61b97c9e5bfef5901
Andi Clemens <andi.clemens@gmail.com>
2024-12-09 14:39:18 +0100

    add comments to setting sections

--- neovim/.config/nvim/lua/core/options.lua
+++ neovim/.config/nvim/lua/core/options.lua
@@ -1,18 +1,23 @@
+-- tabs and indentations
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 
+-- ui and layout
 vim.opt.laststatus = 2
-vim.opt.autoread = true
-vim.opt.autowrite = true
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- file handling
+vim.opt.autoread = true
+vim.opt.autowrite = true
+
+-- search settings
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- map leader to SPACE
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
