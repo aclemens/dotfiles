cac1c2f2844adccdd93748056201baeec91f2b06
Andi Clemens <andi.clemens@gmail.com>
2024-12-16 09:59:48 +0100

    do not update all packages, only the package list

--- installation.sh
+++ installation.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Update pacman package list and all installed packages
-sudo pacman -Syyu
-
 # Install yay for easy AUR package installation
-sudo pacman -S --needed yay
+sudo pacman -Syy --needed yay
 
 # Install all the packages from the packages.txt file
 yay -S --needed - < packages.txt
