8f0e8d2d80389a9b0161cd86c531a8abe509d3d0
Andi Clemens <andi.clemens@gmail.com>
2025-01-02 13:43:35 +0100

    add keymap for searching text under cursor

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -38,6 +38,7 @@ return {
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
     vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fg", builtin.grep_string, { desc = "Telescope search text under cursor" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
   end,
