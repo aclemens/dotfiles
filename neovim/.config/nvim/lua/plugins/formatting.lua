f2cc3a522d90114fc8489da956249026dd702f7c
Andi Clemens <andi.clemens@gmail.com>
2025-01-06 18:39:52 +0100

    add shfmt for formatting shell scripts

--- neovim/.config/nvim/lua/plugins/formatting.lua
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -6,6 +6,7 @@ return {
 
     conform.setup({
       formatters_by_ft = {
+        bash = { "shfmt" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
