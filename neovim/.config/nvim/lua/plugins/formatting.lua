fe8aef2664d282b9ded3ccd957cfa25172e10bc5
Andi Clemens <andi.clemens@gmail.com>
2025-06-02 11:50:31 +0200

    set formatting to async and increase the timeout to 5 seconds

--- neovim/.config/nvim/lua/plugins/formatting.lua
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -39,8 +39,8 @@ return {
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({
         lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
+        async = true,
+        timeout_ms = 5000,
       })
     end, { desc = "Format file or range (in visual mode)" })
   end,
