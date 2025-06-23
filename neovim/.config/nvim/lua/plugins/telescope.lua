43ef8be75ba81b2514af3fffa90114639ace1d29
Andi Clemens <andi.clemens@gmail.com>
2025-06-23 15:45:39 +0200

    move telescope related keymaps (also LSP) to telescope config

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -47,20 +47,20 @@ return {
 
     local builtin = require("telescope.builtin")
 
+    -- find commands
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-    vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope grep" })
     vim.keymap.set("n", "<leader>fg", builtin.grep_string, { desc = "Telescope search text under cursor" })
+    vim.keymap.set("n", "<leader>fi", builtin.current_buffer_fuzzy_find, { desc = "Telescope current buffer grep" })
+    -- LSP
+    vim.keymap.set("n", "<leader>fo", builtin.lsp_document_symbols, { desc = "Telescope LSP document symbols" })
+    vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope LSP references" })
+    vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { desc = "Telescope LSP definitions" })
+    vim.keymap.set("n", "<leader>ft", builtin.lsp_type_definitions, { desc = "Telescope LSP type definitions" })
+    -- Diagnostics
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-    vim.keymap.set(
-      "n",
-      "<leader>fi",
-      builtin.current_buffer_fuzzy_find,
-      { desc = "Telescope current buffer fuzzy find" }
-    )
     vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Telescope commands" })
     vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
-    vim.keymap.set("n", "<leader>fo", builtin.lsp_document_symbols, { desc = "Telescope LSP document symbols" })
-    vim.keymap.set("n", "<leader>fO", builtin.lsp_workspace_symbols, { desc = "Telescope LSP workspace symbols" })
   end,
 }
