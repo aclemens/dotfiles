c159d89bfbcd35c451f577b31444c8884737d2e4
Andi Clemens <andi.clemens@gmail.com>
2025-07-06 12:48:19 +0200

    move keymap definition out of the setup function

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -10,17 +10,14 @@ return {
       -- See Configuration section for options
     },
     config = function()
-      local chat = require("CopilotChat")
+      require("CopilotChat").setup()
 
-      chat.setup({
-        vim.keymap.set("n", "<leader>co", ":CopilotChatOpen<CR>", { desc = "Open Copilot Chat" }),
-        vim.keymap.set("n", "<leader>cc", ":CopilotChatClose<CR>", { desc = "Close Copilot Chat" }),
-        vim.keymap.set("n", "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Explain the code (last selection)" }),
-        vim.keymap.set("v", "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Explain the selected code" }),
-        vim.keymap.set("n", "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Create documentation (current line)" }),
-        vim.keymap.set("v", "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Create documentation for selected code" }),
-      })
+      vim.keymap.set("n", "<leader>co", ":CopilotChatOpen<CR>", { desc = "Open Copilot Chat" })
+      vim.keymap.set("n", "<leader>cc", ":CopilotChatClose<CR>", { desc = "Close Copilot Chat" })
+      vim.keymap.set("n", "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Explain the code (last selection)" })
+      vim.keymap.set("v", "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Explain the selected code" })
+      vim.keymap.set("n", "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Create documentation (current line)" })
+      vim.keymap.set("v", "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Create documentation for selected code" })
     end,
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
