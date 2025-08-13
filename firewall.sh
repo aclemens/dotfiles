1ba10c5072b9b88a5cf55a295d097d25399b9cbf
Andi Clemens <andi.clemens@gmail.com>
2025-08-13 12:06:53 +0200

    add script for enabling ufw firewall

---
+++ firewall.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+# setup ufw
+sudo pacman -S --noconfirm --needed ufw
+
+# disable ufw if it is already enabled
+sudo ufw disable
+# reset ufw to default settings
+sudo ufw reset
+
+# deny all incoming connections
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw default deny routed
+
+# # QMENU settings
+# Allow DNS
+sudo ufw route allow in on virbr0 out on wlan0 proto udp to any port 53
+# Allow HTTP
+sudo ufw route allow in on virbr0 out on wlan0 proto tcp to any port 80
+# Allow HTTPS
+sudo ufw route allow in on virbr0 out on wlan0 proto tcp to any port 443
+
+# enable ufw
+sudo ufw enable
+sudo ufw reload
+
+# show ufw status
+sudo ufw status verbose
