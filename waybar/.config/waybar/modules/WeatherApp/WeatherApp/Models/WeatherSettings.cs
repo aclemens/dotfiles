de275f4f542215fad0eb2cffa8104e4e6cb6f99c
Andi Clemens <andi.clemens@gmail.com>
2026-01-29 00:46:57 +0100

    remove format settings

--- waybar/.config/waybar/modules/WeatherApp/WeatherApp/Models/WeatherSettings.cs
+++ waybar/.config/waybar/modules/WeatherApp/WeatherApp/Models/WeatherSettings.cs
@@ -3,7 +3,5 @@ namespace WeatherApp.Models;
 public class WeatherSettings
 {
     public string TextLocation { get; set; } = "Bensheim";
-    public string TextFormat { get; set; } = "%t+%C";
     public List<string> TooltipLocations { get; set; } = new();
-    public string TooltipFormat { get; set; } = "%l:%c%t+(%C)\\n";
 }
