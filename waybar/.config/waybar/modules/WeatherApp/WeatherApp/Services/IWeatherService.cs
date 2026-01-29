c7bf36aa00b14668f18646288bdc78a042596e0e
Andi Clemens <andi.clemens@gmail.com>
2026-01-29 10:25:46 +0100

    add comment

--- waybar/.config/waybar/modules/WeatherApp/WeatherApp/Services/IWeatherService.cs
+++ waybar/.config/waybar/modules/WeatherApp/WeatherApp/Services/IWeatherService.cs
@@ -4,5 +4,10 @@ namespace WeatherApp.Services;
 
 public interface IWeatherService
 {
+    /// <summary>
+    /// Retrieves weather data for the specified locations.
+    /// </summary>
+    /// <param name="locations">A collection of location names.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the weather data.</returns>
     Task<WeatherData> GetWeatherAsync(IEnumerable<string> locations);
 }
