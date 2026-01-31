21aefc7d8bba9b931b4056c9cff9701514061540
Andi Clemens <andi.clemens@gmail.com>
2026-01-31 11:42:51 +0100

    remove default value

--- waybar/.config/waybar/modules/WeatherApp/WeatherApp/Models/WeatherSettings.cs
+++ waybar/.config/waybar/modules/WeatherApp/WeatherApp/Models/WeatherSettings.cs
@@ -2,6 +2,6 @@ namespace WeatherApp.Models;
 
 public class WeatherSettings
 {
-    public string TextLocation { get; set; } = "Bensheim";
+    public string TextLocation { get; set; } = string.Empty;
     public List<string> TooltipLocations { get; set; } = new();
 }
